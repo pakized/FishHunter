efc52555580906e3bbcf24fd096927bc904fb957
pakize <pduran@web.de>
2026-04-22 13:19:18 +0200

    Walking animation eingefügt

--- neues-spiel/Scripts/killzone.gd
+++ neues-spiel/Scripts/killzone.gd
@@ -5,9 +5,12 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free() 	#remove collision from body 
 	timer.start()
 	
 	
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- neues-spiel/Scripts/player.gd
+++ neues-spiel/Scripts/player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-@onready var sprite = $AnimatedSprite2D
+@onready var playerSprite = $AnimatedSprite2D
 
 func _ready():
-	sprite.play("idle")
+	playerSprite.play("walk")
 
 
 func _physics_process(delta: float) -> void:
@@ -16,12 +15,20 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	#input direction can be 1 0 -1
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		playerSprite.play("walk")
+	if direction < 0:
+		playerSprite.play("walk_left")
+	if direction == 0:
+		playerSprite.play("idle")
+
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
