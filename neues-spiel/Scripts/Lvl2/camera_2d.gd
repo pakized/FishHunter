050f298ced1a75b039a8e130d7eb66ded36929d6
pakize <pduran@web.de>
2026-04-23 22:26:46 +0200

    add new camera with same zoom in new scene

---
+++ neues-spiel/Scripts/Lvl2/camera_2d.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	zoom = Vector2(4,4)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_position = PlayerCharakter.global_position
+	pass
