0d0dfb652d93b3c100372883e0f62abd88f363d1
pakize <pduran@web.de>
2026-04-29 23:03:35 +0200

    mod snowball so that boss not killable with simple snowball

--- neues-spiel/Scripts/lvl3/snowball.gd
+++ neues-spiel/Scripts/lvl3/snowball.gd
@@ -26,7 +26,7 @@ func _physics_process(delta):
 func _on_area_entered(area: Area2D) -> void:
 	print("body entered: " + area.name)
 	print("Grouü " , area.get_groups())
-	if area.is_in_group("Enemy"):
+	if area.is_in_group("Enemy") and not area.is_in_group("Boss"):
 		print("test")
 		area.get_parent().queue_free()
 		queue_free()
