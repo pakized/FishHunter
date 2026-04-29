476d604f0725d4300cd5d661df8f3cc937f6821e
pakize <pduran@web.de>
2026-04-29 13:17:37 +0200

    bugFix SceneTrigger now works on level 3 to level 4

--- neues-spiel/Scripts/scene_trigger.gd
+++ neues-spiel/Scripts/scene_trigger.gd
@@ -7,11 +7,10 @@ var scene_folder = "res://Levels/"
 #res://Levels/level_2.tscn 
 
 #var levelNames: Array[String] = ["Game", "level2", "level_3"]
-var scenePaths: Array[String] = ["res://Scenes/Game.tscn", "res://Scenes/Levels/Level2/level_2.tscn", "res://Scenes/Levels/Level3/level_3.tscn"]
+var scenePaths: Array[String] = ["res://Scenes/Game.tscn", "res://Scenes/Levels/Level2/level_2.tscn", "res://Scenes/Levels/Level3/level_3.tscn", "res://Scenes/Levels/level4/level_4.tscn", "res://Scenes/Game.tscn"]
 
 func _on_body_entered(body: Node2D) -> void:
 	#var fullPath = scene_folder + connected_scene + ".tscn"
-	
 	if body is Player:
 		if GameManager.score == 8:
 			match get_tree().current_scene.name:
@@ -19,7 +18,7 @@ func _on_body_entered(body: Node2D) -> void:
 					scene_manager.change_scene(scenePaths[1], body)
 				"level_2":
 					scene_manager.change_scene(scenePaths[2], body)
-				"level3":
+				"Level3":
 					scene_manager.change_scene(scenePaths[3], body)
 				"level4":
 					scene_manager.change_scene(scenePaths[4], body)
